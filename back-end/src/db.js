c0df36ab6d2a0a8f2fad63f5351bc5501910dc5e
SeanCassone <sean.cassone@gmail.com>
2021-07-21 12:32:45 -0400

    set up login and signup page

--- back-end/src/db.js
+++ back-end/src/db.js
@@ -1,15 +1,19 @@
-import { MongoClient } from 'mongodb';
+import { MongoClient } from "mongodb";
 
 let client;
 
 export const initializeDbConnection = async () => {
-    client = await MongoClient.connect('mongodb://localhost:27017', {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
+  try {
+    client = await MongoClient.connect("mongodb://localhost:27017", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     });
-}
+  } catch {
+    console.error();
+  }
+};
 
-export const getDbConnection = dbName => {
-    const db = client.db(dbName);
-    return db;
-}
+export const getDbConnection = (dbName) => {
+  const db = client.db(dbName);
+  return db;
+};
